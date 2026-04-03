5d2b824587a1cb27dc22cf46b0961d34428422f0
Samuel Letellier-Duchesne <sam@carbonsignal.com>
2026-04-03 12:51:05 -0400

    fix: correct environment variable case for Windows drive root detection

--- src/idfkit/simulation/config.py
+++ src/idfkit/simulation/config.py
@@ -321,7 +321,7 @@ def _platform_search_dirs() -> list[Path]:
             if val:
                 dirs.append(Path(val))
         # Default EnergyPlus installer places dirs at drive root (e.g. C:\EnergyPlusV25-2-0)
-        drive = os.environ.get("SystemDrive", "C:")
+        drive = os.environ.get("SYSTEMDRIVE", "C:")
         dirs.append(Path(drive + os.sep))
 
     return dirs
